3e1601ff06136ddcd1e6070892a8ec5e84846587
maru <p_sso_0@ewhain.net>
2025-01-31 23:02:41 +0900

    섹션4: 새로운 할인 정책 적용과 문제점

--- maru/core/src/main/java/kumaru/core/order/OrderServiceImpl.java
+++ maru/core/src/main/java/kumaru/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package kumaru.core.order;
 
 import kumaru.core.discount.DiscountPolicy;
 import kumaru.core.discount.FixDiscountPolicy;
+import kumaru.core.discount.RateDiscountPolicy;
 import kumaru.core.member.Member;
 import kumaru.core.member.MemberRepository;
 import kumaru.core.member.MemoryMemberRepository;
@@ -9,7 +10,9 @@ import kumaru.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     public final MemberRepository memberRepository = new MemoryMemberRepository();
-    public final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    public final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    public final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    public DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
