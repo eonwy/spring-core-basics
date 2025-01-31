17fb6747ba138fd002d82b73ed6eaa1eb8a3a21d
maru <p_sso_0@ewhain.net>
2025-01-31 23:56:03 +0900

    섹션4: AppConfig 리팩토링

--- maru/core/src/main/java/kumaru/core/AppConfig.java
+++ maru/core/src/main/java/kumaru/core/AppConfig.java
@@ -1,6 +1,8 @@
 package kumaru.core;
 
+import kumaru.core.discount.DiscountPolicy;
 import kumaru.core.discount.FixDiscountPolicy;
+import kumaru.core.discount.RateDiscountPolicy;
 import kumaru.core.member.MemberService;
 import kumaru.core.member.MemberServiceImpl;
 import kumaru.core.member.MemoryMemberRepository;
@@ -9,11 +11,20 @@ import kumaru.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 생성자 주입
+        return new MemberServiceImpl(memberRepository()); // 생성자 주입
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
