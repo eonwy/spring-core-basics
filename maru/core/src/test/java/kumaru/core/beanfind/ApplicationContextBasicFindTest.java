a9e828ff2bb98a60bd2a0e7164dfc8ff0f4ff7c7
maru <p_sso_0@ewhain.net>
2025-02-06 22:38:23 +0900

    섹션5: 처음~스프링 빈 조회-기본

---
+++ maru/core/src/test/java/kumaru/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,52 @@
+package kumaru.core.beanfind;
+
+import kumaru.core.AppConfig;
+import kumaru.core.member.MemberService;
+import kumaru.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+//    인터페이스로 조회하면 인터페이스의 구현체가 조회 대상이 됨
+
+
+//    구체 타입으로 조회 시 유연성 떨어짐
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+
+    @Test
+    @DisplayName("빈 이름으로 조회 X")
+    void findBeanByNameX() {
+//        ac.getBean("xxxxx", MemberService.class);
+//        assertThroes는 junit의 Assertions가 제공.
+        assertThrows(NoSuchBeanDefinitionException.class,
+                ()-> ac.getBean("xxxxx", MemberService.class));
+    }
+}
