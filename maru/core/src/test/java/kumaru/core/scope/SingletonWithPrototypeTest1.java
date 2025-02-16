38ee64884404b9f313edfec8de5fb1714aab4590
maru <p_sso_0@ewhain.net>
2025-02-16 22:48:23 +0900

    섹션10: 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결

--- maru/core/src/test/java/kumaru/core/scope/SingletonWithPrototypeTest1.java
+++ maru/core/src/test/java/kumaru/core/scope/SingletonWithPrototypeTest1.java
@@ -2,10 +2,10 @@ package kumaru.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import org.assertj.core.api.Assertions;
+import jakarta.inject.Provider;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -34,23 +34,26 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입
-
-//        @Autowired
-//        ApplicationContext applicationContext;
+//        private final PrototypeBean prototypeBean; // 생성 시점에 주입
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider; // ObjectFactory 상속
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider; // ObjectFactory 상속
+//        private ObjectFactory<PrototypeBean> prototypeBeanProvider;
+
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
 
         public int logic() {
-//            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
