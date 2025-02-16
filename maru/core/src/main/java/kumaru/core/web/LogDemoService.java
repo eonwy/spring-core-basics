f60f5a3e954e62dc43f62a5c6839feb49d138c26
maru <p_sso_0@ewhain.net>
2025-02-16 23:25:18 +0900

    섹션10: request 스코프 예제 만들기-스코프와 Provider

---
+++ maru/core/src/main/java/kumaru/core/web/LogDemoService.java
@@ -0,0 +1,19 @@
+package kumaru.core.web;
+
+import kumaru.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.log("service id = " + id);
+
+    }
+}
