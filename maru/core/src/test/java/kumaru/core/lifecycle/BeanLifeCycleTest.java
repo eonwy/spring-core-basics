9699676a06f0d273b1dfe4a2f70e8a3ee36b8113
maru <p_sso_0@ewhain.net>
2025-02-16 20:05:58 +0900

    섹션9: 빈 생명주기 콜백 시작

---
+++ maru/core/src/test/java/kumaru/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,29 @@
+package kumaru.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+//        AnnotationConfigApplicationContext 또는 그의 상위에 있는 ConfigurableApplicationContext 사용
+//        ApplicationContext에서는 close 제공하지 않음
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
