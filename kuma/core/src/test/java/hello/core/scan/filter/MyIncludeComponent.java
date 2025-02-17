7acbfa0c2e263fb97537e48eef7e0ed4feb1910c
kuma <yeawon6566@gmail.com>
2025-02-17 21:32:28 +0900

    섹션7 : 컴포넌트 스캔 완료

---
+++ kuma/core/src/test/java/hello/core/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,9 @@
+package hello.core.scan.filter;
+
+import java.lang.annotation.Target;
+
+@java.lang.annotation.Target({java.lang.annotation.ElementType.TYPE})
+@java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@java.lang.annotation.Documented
+public @interface MyIncludeComponent {
+}
